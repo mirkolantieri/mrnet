abcf85bf7de6c54aa42b5a3287dc222c5eeb227e
mirkolantieri <mirklan@outlook.it>
2021-05-10 15:56:17 +0200

    Created new dcgan file; dataset converted to .jpg images;

--- dcgan.py
+++ dcgan.py
@@ -7,11 +7,11 @@
 # 
 # 
 
-from __future__ import barry_as_FLUFL, print_function
+# Importing libraries
+
+from __future__ import print_function
 
 #%matplotlib inline
-import argparse
-import os
 import random
 
 import matplotlib.animation as animation
@@ -26,7 +26,8 @@ import torch.utils.data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
-from loader import MRDataset
+from IPython.display import HTML
+from skimage.io import imsave
 
 # Set random seed for reproducibility
 manualSeed = 999
@@ -35,6 +36,8 @@ print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 
+# Root directory for dataset
+dataroot = "./output/"
 # Number of workers for dataloader
 workers = 2
 # Batch size during training
@@ -68,7 +71,6 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
-
 
 # Generator Class
 
@@ -133,34 +135,31 @@ class Discriminator(nn.Module):
         return self.main(input)
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--task', type=str, required=True,
-                        choices=['abnormal', 'acl', 'meniscus'])
-    parser.add_argument('-p', '--plane', type=str, required=True,
-                        choices=['sagittal', 'coronal', 'axial'])
-    args = parser.parse_args()
-    return args
-
-if __name__ == "__main__":
-    args = parse_arguments()
-    
-    train_dataset = MRDataset('./data/', args.task, args.plane, train=True)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=workers, drop_last=False)
-
-    validation_dataset = MRDataset('./data/', args.task, args.plane, train=False)
-    validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=1, shuffle=-True, num_workers=2, drop_last=False)
+def main():
+    # We can use an image folder dataset the way we have it setup.
+    # Create the dataset
+    dataset = dset.ImageFolder(root=dataroot,
+                            transform=transforms.Compose([
+                                transforms.Resize(image_size),
+                                transforms.CenterCrop(image_size),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            ]))
+    # Create the dataloader
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                            shuffle=True, num_workers=workers)
 
     # Decide which device we want to run on
     device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
     # Plot some training images
-    #real_batch = next(iter(train_loader))
-    #plt.figure(figsize=(8,8))
-    #plt.axis("off")
-    #plt.title("Training Images")
-    #plt.show()
-    #plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+    real_batch = next(iter(dataloader))
+    plt.figure(figsize=(8,8))
+    plt.axis("off")
+    plt.title("Training Images")
+    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+    plt.show()
+
 
     # Create the generator
     netG = Generator(ngpu).to(device)
@@ -175,7 +174,6 @@ if __name__ == "__main__":
 
     # Print the model
     print(netG)
-
     # Create the Discriminator
     netD = Discriminator(ngpu).to(device)
 
@@ -205,6 +203,7 @@ if __name__ == "__main__":
     optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
     optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
 
+
     # Training Loop
 
     # Lists to keep track of progress
@@ -212,16 +211,15 @@ if __name__ == "__main__":
     G_losses = []
     D_losses = []
     iters = 0
-    
-    print("Starting Training Loop...")
 
+    print("Starting Training Loop...")
     # For each epoch
     for epoch in range(num_epochs):
         # For each batch in the dataloader
-        for i, data in enumerate(train_loader, 0):
-            if i == 420: break
+        for i, data in enumerate(dataloader, 0):
+
             ############################
-            # (1) Update D network
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
             ###########################
             ## Train with all-real batch
             netD.zero_grad()
@@ -271,9 +269,9 @@ if __name__ == "__main__":
             optimizerG.step()
 
             # Output training stats
-            if i % 20 == 0:
+            if i % 50 == 0:
                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                    % (epoch, num_epochs, i, len(train_loader),
+                    % (epoch, num_epochs, i, len(dataloader),
                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
             # Save Losses for plotting later
@@ -281,9 +279,50 @@ if __name__ == "__main__":
             D_losses.append(errD.item())
 
             # Check how the generator is doing by saving G's output on fixed_noise
-            if (iters % 420 == 0) or ((epoch == num_epochs-1) and (i == len(train_loader)-1)):
+            if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
                 with torch.no_grad():
                     fake = netG(fixed_noise).detach().cpu()
                 img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
             iters += 1
+    
+    plt.figure(figsize=(10,5))
+    plt.title("Generator and Discriminator Loss During Training")
+    plt.plot(G_losses,label="G")
+    plt.plot(D_losses,label="D")
+    plt.xlabel("iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+
+    #%%capture
+    fig = plt.figure(figsize=(8,8))
+    plt.axis("off")
+    ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+
+    HTML(ani.to_jshtml())
+
+    # Grab a batch of real images from the dataloader
+    real_batch = next(iter(dataloader))
+
+    # Plot the real images
+    plt.figure(figsize=(15,15))
+    plt.subplot(1,2,1)
+    plt.axis("off")
+    plt.title("Real Images")
+    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+
+    # Plot the fake images from the last epoch
+    plt.subplot(1,2,2)
+    plt.axis("off")
+    plt.title("Fake Images")
+    plt.imshow(np.transpose(img_list[-1],(1,2,0)))
+    plt.show()
+
+
+    for i in range(len(img_list)):
+        imsave(f'./gans/gan_{i}.jpg', img_list[i])
+
+if __name__ == "__main__":
+    main()
