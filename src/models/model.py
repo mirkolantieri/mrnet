ca8522bead3d53aa1225977029ec4f01eb6126cf
mirkolantieri <mirklan@outlook.it>
2021-06-08 14:05:52 +0200

    ...

---
+++ src/models/model.py
@@ -0,0 +1,39 @@
+#
+# 2021 (c) by **Mirko Lantieri**
+# All rights reserved.
+#
+# model.py : script responsable for the creation of the AlexNet CNN model
+# The file contains the respective class: AlexNet(nn.Module) inheritates from the nn.Module parent class
+#
+
+
+# Importing libraries
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torchvision import models
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+# Reserved space for the implementation of the AlexNet based AI CNN (optimized already)
+
+
+class AlexNet(nn.Module):
+    """ class `AlexNet`: the implementation of the optimized AlexNet modern convolutional neural network
+    to be used for the training and prediction. \\
+    Weights initilization with "Imagenet"
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.model = models.alexnet(pretrained=True)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.classifier = nn.Linear(256, 1)
+
+    def forward(self, x):
+        x = torch.squeeze(x, dim=0)
+        x = self.model.features(x)
+        x = self.avg_pool(x).view(x.size(0), -1)
+        x = torch.max(x, 0, keepdim=True)[0]
+        x = self.classifier(x)
+        return x
---
+++ src/training/trainer_wu.sh
@@ -0,0 +1,19 @@
+DATE=$(date +"%Y-%m-%d-%H-%M")
+EXPERIMENT="exp_wu"
+
+EPOCHS=20
+PREFIX="mrnet_wu"
+
+python train_wu.py -t acl -p sagittal --prefix_name $PREFIX --epochs=$EPOCHS --experiment $EXPERIMENT
+python train_wu.py -t acl -p coronal --prefix_name $PREFIX --epochs=$EPOCHS  --experiment $EXPERIMENT
+python train_wu.py -t acl -p axial --prefix_name $PREFIX --epochs=$EPOCHS --experiment $EXPERIMENT
+
+python train_wu.py -t meniscus -p sagittal --prefix_name $PREFIX --epochs=$EPOCHS --experiment $EXPERIMENT
+python train_wu.py -t meniscus -p coronal --prefix_name $PREFIX --epochs=$EPOCHS --experiment $EXPERIMENT
+python train_wu.py -t meniscus -p axial --prefix_name $PREFIX --epochs=$EPOCHS --experiment $EXPERIMENT
+
+python train_wu.py -t abnormal -p sagittal --prefix_name $PREFIX --epochs=$EPOCHS --experiment $EXPERIMENT
+python train_wu.py -t abnormal -p coronal --prefix_name $PREFIX --epochs=$EPOCHS --experiment $EXPERIMENT
+python train_wu.py -t abnormal -p axial --prefix_name $PREFIX --epochs=$EPOCHS --experiment $EXPERIMENT
+
+
