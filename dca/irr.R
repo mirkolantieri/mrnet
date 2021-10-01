d74ddf33409ac1b5bfbccbc508619d7bf64699d6
mirkolantieri <mirklan@outlook.it>
2021-10-01 17:58:03 +0200

    Added inter-rate agreement

--- dca/irr.R
+++ dca/irr.R
@@ -1,8 +1,9 @@
 #install.packages("irr")
+#install.packages("tidyverse")
 library(irr)
+library(tidyverse)
 
-
-# Complex Model AUC
+# Complex Model AUC - Model WU
 men_lab <- read.csv2('./results (model auc)/Complex/complex-meniscus-label.csv', sep = ',')
 men_pred <- read.csv2('./results (model auc)/Complex/complex-meniscus-prediction.csv', sep = ',')
 
@@ -12,6 +13,119 @@ abn_pred <- read.csv2('./results (model auc)/Complex/complex-abnormal-prediction
 acl_lab <- read.csv2('./results (model auc)/Complex/complex-acl-label.csv', sep = ',')
 acl_pred <- read.csv2('./results (model auc)/Complex/complex-acl-prediction.csv', sep = ',')
 
-men <- merge(men_lab, men_pred, by="Case", all=TRUE)
-ratings <- select(men$Score.x, men$Score.y)
+meniscus_lab_wu <- read.csv2('./results (model wu)/Complex/complex-meniscus-label.csv', sep = ',')
+meniscus_pred_wu <- read.csv2('./results (model wu)/Complex/complex-meniscus-prediction.csv', sep = ',')
 
+abn_lab_wu <- read.csv2('./results (model wu)/Complex/complex-abnormal-label.csv', sep = ',')
+abn_pred_wu <- read.csv2('./results (model wu)/Complex/complex-abnormal-prediction.csv', sep = ',')
+
+acl_lab_wu <- read.csv2('./results (model wu)/Complex/complex-acl-label.csv', sep = ',')
+acl_pred_wu <- read.csv2('./results (model wu)/Complex/complex-acl-prediction.csv', sep = ',')
+
+
+men_wu <- merge(meniscus_lab_wu, meniscus_pred_wu, by = "Case", all = TRUE)
+abn_wu <- merge(abn_lab_wu, abn_pred_wu, by = "Case", all = TRUE)
+acl_wu <- merge(acl_lab_wu, acl_pred_wu, by = "Case", all = TRUE)
+
+men <- merge(men_lab, men_pred, by="Case", all=TRUE)
+abn <- merge(abn_lab, abn_pred, by="Case", all=TRUE)
+acl <- merge(acl_lab, acl_pred, by="Case", all=TRUE)
+
+men_ratings <- men %>% select(Score.x, Score.y)
+abn_ratings <- abn %>% select(Score.x, Score.y)
+acl_ratings <- acl %>% select(Score.x, Score.y)
+
+men_ratings_wu <- men_wu %>% select(Score.x, Score.y)
+abn_ratings_wu <- abn_wu %>% select(Score.x, Score.y)
+acl_ratings_wu <- acl_wu %>% select(Score.x, Score.y)
+
+
+# Overall agreement
+print("Model Blue Overall Agreement - Complex Case")
+agree(men_ratings)
+agree(abn_ratings)
+agree(acl_ratings)
+
+print("Model Green Overall Agreement - Complex Case")
+agree(men_ratings_wu)
+agree(abn_ratings_wu)
+agree(acl_ratings_wu)
+
+
+# Cohen's Kappa
+kappa2(men_ratings)
+kappa2(abn_ratings)
+kappa2(acl_ratings)
+
+kappa2(men_ratings_wu)
+kappa2(abn_ratings_wu)
+kappa2(acl_ratings_wu)
+
+
+# Krippendorff's alpha
+kripp.alpha(men_ratings, method = "ordinal")
+kripp.alpha(abn_ratings, method = "ordinal")
+kripp.alpha(acl_ratings, method = "ordinal")
+
+# Validation Model AUC - WU
+
+men_test_lab <- read.csv2('./results (model auc)/Test Set/meniscus-label.csv', sep = ',')
+men_test_pred <- read.csv2('./results (model auc)/Test Set/meniscus-prediction.csv', sep = ',')
+
+abn_test_lab <- read.csv2('./results (model auc)/Test Set/abnormal-label.csv', sep = ',')
+abn_test_pred <- read.csv2('./results (model auc)/Test Set/abnormal-prediction.csv', sep = ',')
+
+acl_test_lab <- read.csv2('./results (model auc)/Test Set/acl-label.csv', sep = ',')
+acl_test_pred <- read.csv2('./results (model auc)/Test Set/acl-prediction.csv', sep = ',')
+
+
+men_test <- merge(men_test_lab, men_test_pred, by="Case", all=TRUE)
+abn_test <- merge(abn_test_lab, abn_test_pred, by = "Case", all=TRUE)
+acl_test <- merge(acl_test_lab, acl_test_pred, by = "Case", all=TRUE)
+
+
+men_test_lab_wu <- read.csv2('./results (model wu)/Test Set/meniscus-label.csv', sep = ',')
+men_test_pred_wu <- read.csv2('./results (model wu)/Test Set/meniscus-prediction.csv', sep = ',')
+
+abn_test_lab_wu <- read.csv2('./results (model wu)/Test Set/abnormal-label.csv', sep = ',')
+abn_test_pred_wu <- read.csv2('./results (model wu)/Test Set/abnormal-prediction.csv', sep = ',')
+
+acl_test_lab_wu <- read.csv2('./results (model wu)/Test Set/acl-label.csv', sep = ',')
+acl_test_pred_wu <- read.csv2('./results (model wu)/Test Set/acl-prediction.csv', sep = ',')
+
+
+men_test_wu <- merge(men_test_lab_wu, men_test_pred_wu, by="Case", all=TRUE)
+abn_test_wu <- merge(abn_test_lab_wu, abn_test_pred_wu, by = "Case", all=TRUE)
+acl_test_wu <- merge(acl_test_lab_wu, acl_test_pred_wu, by = "Case", all=TRUE)
+
+
+men_ratings_test <- men_test %>% select(Score.x, Score.y)
+abn_ratings_test <- abn_test %>% select(Score.x, Score.y)
+acl_ratings_test <- acl_test %>% select(Score.x, Score.y)
+
+men_ratings_test_wu <- men_test_wu %>% select(Score.x, Score.y)
+abn_ratings_test_wu <- abn_test_wu %>% select(Score.x, Score.y)
+acl_ratings_test_wu <- acl_test_wu %>% select(Score.x, Score.y)
+
+
+# Overall agreement
+print("Model Blue Overall Agreement - Validation Set")
+
+agree(men_ratings_test)
+agree(abn_ratings_test)
+agree(acl_ratings_test)
+
+print("Model Green Overall Agreement - Validation Set")
+agree(men_ratings_test_wu)
+agree(abn_ratings_test_wu)
+agree(acl_ratings_test_wu)
+
+
+# Cohen's Kappa
+kappa2(men_ratings_test)
+kappa2(abn_ratings_test)
+kappa2(acl_ratings_test)
+
+kappa2(men_ratings_test_wu)
+kappa2(abn_ratings_test_wu)
+kappa2(acl_ratings_test_wu)
